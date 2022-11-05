9c8b2fd6540355669f64fffe7f4d313707b6079b
mye8503 <mye8503@gmail.com>
2022-11-05 17:31:01 -0400

    attempted pdf2image

---
+++ src/pdf2img.py
@@ -0,0 +1,12 @@
+
+# import module
+from pdf2image import convert_from_path
+ 
+ 
+# Store Pdf with convert_from_path function
+images = convert_from_path('345syllabus.pdf')
+ 
+for i in range(len(images)):
+   
+      # Save pages as images in the pdf
+    images[i].save('page'+ str(i) +'.jpg', 'JPEG')
