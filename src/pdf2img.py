a7c7dd29496539329d61f96dce39178de24c3bf3
Harshit Sohaney <harshit.sohaney@mail.utoronto.ca>
2022-11-05 20:18:59 -0400

    tried to fix pdf parser

--- src/pdf2img.py
+++ src/pdf2img.py
@@ -11,21 +11,22 @@ sys.path
 sys.executable
 
 
-# import PyPDF2
- 
+import PyPDF2
+import pdf2image as p2i
+
 #create file object variable
 #opening method will be rb
-pdffileobj=open('src/345syllabus.pdf','rb')
+pdffileobj=open(r'D:\VScode\NewHacks-2022\src\345syllabus.pdf','rb')
  
 #create reader variable that will read the pdffileobj
 pdfreader=PyPDF2.PdfFileReader(pdffileobj)
  
 # Store Pdf with convert_from_path function
-images = convert_from_path('345syllabus.pdf')
+images = p2i.convert_from_path('345syllabus.pdf')
  
 #(x+1) because python indentation starts with 0.
 #create text variable which will store all text datafrom pdf file
-text=pageobj.extractText()
+text= p2i.pageobj.extractText()
  
 #save the extracted data from pdf to a txt file
 #we will use file handling here
@@ -33,5 +34,5 @@ text=pageobj.extractText()
 #go to the file location copy the path by right clicking on the file
 #click properties and copy the location path and paste it here.
 #put "\\your_txtfilename"
-file1=open(r"C:\Users\SIDDHI\AppData\Local\Programs\Python\Python38\\1.txt","a")
+file1=open(r'1.txt',"a")
 file1.writelines(text)
